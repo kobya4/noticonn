a76cf59db52ea74cc566ec4e2b0c2b30de237922
Masamune Kobayashi <kobya4@gmail.com>
2019-09-29 02:16:22 +0900

    :tada: バックグラウンドでのAPIのfetchとプッシュ通知実装

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,59 @@
-const test = () => {};
+import { API_BASE_URL, TOPICS_STORAGE_KEY } from "./constants";
+import axios from "axios";
 
-export default test;
+type Event = {
+  id: number;
+  title: string;
+  author: string;
+  url: string;
+};
+
+type MockResponse = Array<Event>;
+
+const pushNotification = (event: Event) => {
+  const options = {
+    iconUrl: "icon128.png",
+    type: "basic",
+    title: `NotiConn | ${event.title}`,
+    message: event.url
+  };
+
+  chrome.notifications.create(event.url, options);
+};
+
+const fetchEvents = async (topics: string[]) => {
+  await axios
+    .get<MockResponse>(`${API_BASE_URL}/posts`, { params: { topics } })
+    .then(response => {
+      const { data: events } = response;
+      events.forEach(event => {
+        pushNotification(event);
+      });
+    });
+};
+
+const main = () => {
+  const periodInMinutes = 1;
+  const alarmName = "fetchEvents";
+  const topics = Object.keys(
+    JSON.parse(localStorage.getItem(TOPICS_STORAGE_KEY) || "{}")
+  );
+
+  chrome.alarms.create(alarmName, { periodInMinutes });
+
+  chrome.alarms.onAlarm.addListener(alarm => {
+    if (alarm.name === alarmName) {
+      if (topics.length === 0) {
+        return;
+      }
+      fetchEvents(topics);
+    }
+  });
+
+  chrome.notifications.onClicked.addListener(notificationId => {
+    chrome.tabs.create({ url: notificationId });
+    chrome.notifications.clear(notificationId);
+  });
+};
+
+main();
