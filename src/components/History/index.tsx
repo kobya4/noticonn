3ce06488d8dff9f2ea21a6b9c2c1e946940733e1
Masamune Kobayashi <kobya4@gmail.com>
2020-04-25 02:55:09 +0900

    :bug: 最近のイベントが0件のとき空のカードが表示される問題の修正

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Event } from "../../model/event";
 import { EVENTS_STORAGE_KEY } from "../../constants";
+import "./style.css";
 
 const History: React.FC = () => {
   const newestEvents: Array<Event> = JSON.parse(
@@ -9,23 +10,27 @@ const History: React.FC = () => {
 
   return (
     <div>
-      {newestEvents.map(event => {
-        return (
-          <div key={event.url} className="card">
-            <div className="card-content">
-              <div className="content">
-                <a href={event.url} target="_blank">
-                  {event.title}
-                </a>
-                <br />
-                <small>
-                  {event.topic}に関連 by {event.owner}
-                </small>
+      {Object.keys(newestEvents[0]).length > 0 ? (
+        newestEvents.map(event => {
+          return (
+            <div key={event.url} className="card">
+              <div className="card-content">
+                <div className="content">
+                  <a href={event.url} target="_blank">
+                    {event.title}
+                  </a>
+                  <br />
+                  <small>
+                    {event.topic}に関連 by {event.owner}
+                  </small>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <p className="event-notfound">最近のイベントはありません</p>
+      )}
     </div>
   );
 };
